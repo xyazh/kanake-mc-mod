8cf65a5e9b07ea02df61a994e597d5710e7ff6c6
xyazh <330237917@qq.com>
2023-12-10 12:40:29 +0800

    push

---
+++ src/main/java/com/xyazh/kanake/block/blocks/platform/TilePlatform.java
@@ -0,0 +1,30 @@
+package com.xyazh.kanake.block.blocks.platform;
+
+import com.xyazh.kanake.block.blocks.test.TileTest;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.AxisAlignedBB;
+
+
+import javax.annotation.Nonnull;
+
+public class TilePlatform extends TileTest implements ITickable{
+    public int age = 4;
+
+    public TilePlatform(){
+        super();
+    }
+
+    @Nonnull
+    public AxisAlignedBB getRenderBoundingBox(){
+        return new AxisAlignedBB(Double.NEGATIVE_INFINITY,Double.NEGATIVE_INFINITY,Double.NEGATIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
+    }
+
+    @Override
+    public void update() {
+        if(this.age--<0){
+            world.getBlockState(pos).getBlock().breakBlock(world,pos,world.getBlockState(pos));
+            world.setBlockState(pos, Blocks.AIR.getDefaultState());
+        }
+    }
+}
