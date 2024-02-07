26338213a7b31f474d79c14bb3ebc361e7ba8e89
xyazh <330237917@qq.com>
2024-02-08 00:25:35 +0800

    push

--- src/main/java/com/xyazh/kanake/Kanake.java
+++ src/main/java/com/xyazh/kanake/Kanake.java
@@ -34,7 +34,7 @@ public class Kanake
 {
     public static final String MODID = "kanake";
     public static final String NAME = "Kannmein na kenndaimahou";
-    public static final String VERSION = "0.1.14.beta";
+    public static final String VERSION = "0.1.20.beta";
 
     public static SimpleNetworkWrapper network;
 
